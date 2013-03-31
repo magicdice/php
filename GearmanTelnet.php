fd072f99eb57dbb35593a746f8427b42aecd9dd6
Patrick Reilly <preilly@wikimedia.org>
2013-03-31 15:15:33 -0700

    add back author line and credits

--- GearmanTelnet.php
+++ GearmanTelnet.php
@@ -1,29 +1,18 @@
 <?php
 
 /**
-* Monitoring Gearman over telnet port 4730
-*
-* So the only way to monitor Gearman is via doing a telnet to port 4730. The
-* current monitoring supported commands are fairly basic.
-* There are plans to include more set of commands in the next release.
-*
-* BigTent Design, Inc.
-*
-* @category BigTent
-* @package GearmanTelnet
-* @copyright Copyright (c) 2009 BigTent Design, Inc. (http://www.bigtent.com)
-* @version 1.1 (Modified by Lior Ben-Kereth)
-*/
-
-/**
+ * Monitoring Gearman over telnet port 4730
+ *
+ * So the only way to monitor Gearman is via doing a telnet to port 4730. The
+ * current monitoring supported commands are fairly basic.
+ * There are plans to include more set of commands in the next release.
+ * @version 1.1 (Modified by Lior Ben-Kereth)
  * A class that contains seperated and aggragated workers and status data from all gearman server in a cluster
  * @author liorbk
  * @author preilly@php.net
  * @category   PHP
  * @package    GearmanTelnet
  * @copyright  Copyright (c) 2013 Patrick Reilly (http://www.facebook.com/preilly)
- * @version    $Id: GearmanTelnet.php 16541 2013-02-31 06:59:03Z preilly $
- *
  */
 class GearmanClusterAdmin
 {
